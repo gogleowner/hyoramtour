b6ee575511f81c9b7e7a423687a6bc3a236c022d
gogleowner <gogleowner@gmail.com>
2019-08-24 11:06:26 +0900

    add button for move to google map application

--- hyoramtour-app/app/src/main/java/io/gogleowner/hyoramtour/MainActivity.kt
+++ hyoramtour-app/app/src/main/java/io/gogleowner/hyoramtour/MainActivity.kt
@@ -1,11 +1,15 @@
 package io.gogleowner.hyoramtour
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.gogleowner.hyoramtour.ui.main.SectionsPagerAdapter
 import io.gogleowner.hyoramtour.ui.main.SharedData
@@ -17,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
 
@@ -42,5 +49,27 @@ class MainActivity : AppCompatActivity() {
         editor.putString("tourSchedule", jacksonObjectMapper().writeValueAsString(SharedData.tourSchedules))
 
         Log.i("MainActivity", "tourSchedule shared prefrences 에 저장 => ${editor.commit()}")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_actions, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            when (item.itemId) {
+                R.id.action_bar_googlemap -> {
+                    val intent = packageManager.getLaunchIntentForPackage("com.google.android.apps.maps")
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+                    startActivity(intent)
+
+                    return true
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
